efad1ae0f7e3f4d971649b755f499770d2362ce4
devLucasMoraes <devlucasmoraes@gmail.com>
2024-10-25 23:07:21 -0300

    feat: Roteamento e Controle

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,16 @@ import "express-async-errors";
 import helmet from "helmet";
 import morgan from "morgan";
 
+import customerRouter from "./routers/customerRouter";
+
 const app = express();
 
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+
+app.use("/customers/", customerRouter);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.send("Hello World");
